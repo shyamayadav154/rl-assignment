ea62714969245854de79b37fdb90f782b70f86cf
shyam <shyamayadav154@gmail.com>
2023-05-28 21:14:17 +0530

    fix: home page not working in production

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,11 +6,11 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return (
-    <ClerkProvider>
-      <Component {...pageProps} />
-    </ClerkProvider>
-  ); 
+    return (
+        <ClerkProvider>
+            <Component {...pageProps} />
+        </ClerkProvider>
+    );
 };
 
 export default api.withTRPC(MyApp);
